56b09c03f6978e71c669de7277a82b28e3bb7a49
Nasir <70495316+abdinaa17@users.noreply.github.com>
2021-04-08 23:54:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -160,4 +160,11 @@ The expression will be evaluated to true, since NULL will be treated as any othe
 
 Note: JavaScript is case-sensitive and here we are using NULL instead of null.
 
+// What will the code below output? Explain your answer.
+
+
+console.log(0.1 + 0.2 == 0.3);
+
+false
+
 // Source https://www.toptal.com/javascript/interview-questions
